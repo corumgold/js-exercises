f85467b038da59358a11c662b762298b5febff3b
Corum Gold <goldcorum@gmail.com>
2021-06-15 16:11:52 -0400

    fixed days of week so they are strings

--- days-of-week/days-of-week.js
+++ days-of-week/days-of-week.js
@@ -1,12 +1,12 @@
 const daysOfWeek = {
-    1: Monday,
-    2: Tuesday,
-    3: Wednesday,
-    4: Thursday,
-    5: Friday,
-    6: Saturday,
-    7: Sunday
-}
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+    7: 'Sunday'
+}'
 
 function returnDay(num) {
     if (num < 1 || num > 7) {
