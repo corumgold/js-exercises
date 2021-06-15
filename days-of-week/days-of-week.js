090ca1bef73dc5a478f3589a96a3d5c9b01a3d82
Corum Gold <goldcorum@gmail.com>
2021-06-15 16:09:28 -0400

    add days of week object

--- days-of-week/days-of-week.js
+++ days-of-week/days-of-week.js
@@ -1,5 +1,15 @@
-function returnDay(num){
-    if (num < 1 || num > 7){
+const daysOfWeek = {
+    1: Monday,
+    2: Tuesday,
+    3: Wednesday,
+    4: Thursday,
+    5: Friday,
+    6: Saturday,
+    7: Sunday
+}
+
+function returnDay(num) {
+    if (num < 1 || num > 7) {
         return null;
     }
 }
