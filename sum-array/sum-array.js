00baa2097b0b43882949c8900bd0469505e0c31c
Corum Gold <goldcorum@gmail.com>
2021-06-27 12:45:15 -0400

    add filter practice

--- sum-array/sum-array.js
+++ sum-array/sum-array.js
@@ -1,10 +1,16 @@
-let total = 0
+// let total = 0
 
-const array = [3,4,1]
-function sumArray(array){
-    for (i = 0; i < array.length(); i++){
-        let total = i + total
-    }
-    
-    console.log(total)
-}
+// function sumArray(array){
+//     for (i = 0; i < array.length; i++){
+//         console.log(i);
+//     }
+
+//     console.log(total)    
+
+// }
+
+const userNames = ['bill', 'samiami', 'd392408342rfi', 'staceysmom1978', 'fionalvr']
+
+const validUserNames = userNames.filter(u => {
+    u.length <= 10;
+})
